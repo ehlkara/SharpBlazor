fb05735cc6ce082e859189228022766ba603997c
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-15 12:00:18 +0300

    added validation in blazor form

--- Sharp_Models/CategoryDto.cs
+++ Sharp_Models/CategoryDto.cs
@@ -1,8 +1,11 @@
-﻿namespace Sharp_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sharp_Models
 {
     public class CategoryDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter name..")]
         public string Name { get; set; }
     }
 }
