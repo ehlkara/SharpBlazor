387419df7a3764722dbbd90dd0687a12d5f64036
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-20 15:43:59 +0300

    added appsettings and product service in webasembly project

---
+++ SharpWeb_Client/Service/IService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Sharp_Models;
+
+namespace SharpWeb_Client.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAll();
+        Task<ProductDto> Get(int productId);
+    }
+}
---
+++ SharpWeb_Client/Service/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Sharp_Models;
+using SharpWeb_Client.Service.IService;
+using SharpWeb_Server.Pages;
+
+namespace SharpWeb_Client.Service
+{
+    public class ProductService : IProductService
+    {
+        private HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ProductDto> Get(int productId)
+        {
+            var response = await _httpClient.GetAsync($"/api/product/{productId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var product = JsonConvert.DeserializeObject<ProductDto>(content);
+                return product;
+            }
+            else
+            {
+                var errorModel = JsonConvert.DeserializeObject<ErrorModelDto>(content);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAll()
+        {
+            var response = await _httpClient.GetAsync("/api/product");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(content);
+                return products;
+            }
+            return new List<ProductDto>();
+        }
+    }
+}
