0a07249a328f89ee7671c0ce2e32d74e94501765
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-25 11:07:18 +0300

    added shopping cart and localstorage

---
+++ SharpWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using SharpWeb_Client.ViewModels;
+
+namespace SharpWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        Task DecrementCart(ShoppingCart cartToDecrement);
+        Task IncrementCart(ShoppingCart cartToAdd);
+    }
+}
---
+++ SharpWeb_Client/ViewModels/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using Sharp_Models;
+
+namespace SharpWeb_Client.ViewModels
+{
+    public class ShoppingCart
+    {
+        public int ProductId { get; set; }
+        public ProductDto Product { get; set; }
+        public int ProductPriceId { get; set; }
+        public ProductPriceDto ProductPrice { get; set; }
+        public int Count { get; set; }
+    }
+}
