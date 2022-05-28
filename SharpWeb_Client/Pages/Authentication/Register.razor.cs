e6134861ab75448540005b0431e7f1745b245d3b
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-28 11:47:14 +0300

    added spliting code in register page

---
+++ SharpWeb_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sharp_Models;
+using SharpWeb_Client.Service.IService;
+
+namespace SharpWeb_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private SignUpRequestDto SignUpRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _autService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await _autService.RegisterUser(SignUpRequest);
+            if (result.IsRegisterationSuccessful)
+            {
+                //registration is successful
+                _navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                //failure
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+            IsProcessing = false;
+        }
+    }
+}
+
