dc8956e4e465ffbf6359a7183636ed8e92054d71
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-28 11:56:51 +0300

    added login proccess in login page

---
+++ SharpWeb_Client/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sharp_Models;
+using SharpWeb_Client.Service.IService;
+
+namespace SharpWeb_Client.Pages.Authentication
+{
+    public partial class Login
+    {
+        private SignInRequestDto SignInRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowSignInErrors { get; set; }
+        public string Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _autService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task LoginUser()
+        {
+            ShowSignInErrors = false;
+            IsProcessing = true;
+            var result = await _autService.Login(SignInRequest);
+            if (result.IsAuthSuccessful)
+            {
+                //registration is successful
+                _navigationManager.NavigateTo("/");
+            }
+            else
+            {
+                //failure
+                Errors = result.ErrorMessage;
+                ShowSignInErrors = true;
+            }
+            IsProcessing = false;
+        }
+    }
+}
