6967ba370bc553f9e568684458468361aae494f6
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-05-30 11:48:53 +0300

    added stripepayment js

---
+++ SharpWeb_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,4 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe("pk_test_51IjKxzEGigLMcIKUeCegpxTLsycpNJYlzGDGm2wl7XE4rF7BqHzJ8yeTNQpsFWgbszY3eO4LAQITP8Ch9djiyGWy00bA6cEqKW");
+    stripe.redirectToCheckout({ sessionId: sessionId });
+}
